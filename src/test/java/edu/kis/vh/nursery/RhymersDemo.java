242ea3fa0dc65777643eee41e22028dda0a548b8
Kruz00 <hubertkrauza@gmail.com>
2022-01-24 02:36:40 +0100

    1.5.1 i merge z poprzednimi

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,14 +7,19 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
-		
+
+		testRhymers(factory);
+
+	}
+
+	private static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-		
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
@@ -27,7 +32,6 @@ class RhymersDemo {
 
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
 	}
 
 }
