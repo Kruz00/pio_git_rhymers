15f494004b199cb9fb0acf6d7128d3a23b86b489
Kruz00 <hubertkrauza@gmail.com>
2022-01-24 10:47:41 +0100

    1.5 calosc, dokumentacja w klasie Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -6,26 +6,46 @@ public class Node {
 	private Node prev;
 	private Node next;
 
+	/**
+	 *
+	 * @param i value of node
+	 */
 	public Node(int i) {
 		this.value = i;
 	}
 
+	/**
+	 *
+	 * @return value of node
+	 */
 	public int getValue() {
 		return value;
 	}
 
+	/**
+	 * @return previous node
+	 */
 	public Node getPrev() {
 		return prev;
 	}
 
+	/**
+	 * @param prev previous node to set
+	 */
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
+	/**
+	 * @return next node
+	 */
 	public Node getNext() {
 		return next;
 	}
 
+	/**
+	 * @param next next node to set
+	 */
 	public void setNext(Node next) {
 		this.next = next;
 	}
