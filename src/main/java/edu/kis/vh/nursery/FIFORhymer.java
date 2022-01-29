e49be1534916add3071148b45d69d32479d8eecf
Kruz00 <hubertkrauza@gmail.com>
2022-01-29 15:30:44 +0100

    2.3.5 + naprawa buga w FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,25 +1,29 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stacks.IntLinkedList;
 import edu.kis.vh.nursery.stacks.IntStack;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
+
+	private final IntStack temp = new IntLinkedList();
 
 	public FIFORhymer(IntStack list) {
 		super(list);
 	}
 
 	public FIFORhymer() {
+		super();
 	}
 
 	@Override
 	public int countOut() {
 		while (!callCheck())
-			super.countIn(super.countOut());
+			temp.push(super.countOut());
 		
-		int ret = super.countOut();
+		int ret = temp.pop();
 		
-		while (!super.callCheck())
-			countIn(super.countOut());
+		while (!temp.isEmpty())
+			countIn(temp.pop());
 		
 		return ret;
 	}
